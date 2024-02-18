f4718266574e22aaa4538268b3e4f9378ba00fb3
Amberits <info@hdsteam.com>
2024-02-18 10:54:41 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import checker from 'vite-plugin-checker'
+import VitePluginBrowserSync from 'vite-plugin-browser-sync'
+import { qrcode } from 'vite-plugin-qrcode';
+
+export default {
+  plugins: [
+   
+    checker({
+      typescript: true,
+       eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+      stylelint: {
+        lintCommand: 'stylelint ./src/**/*.{css,scss}'
+      },
+    }),
+     VitePluginBrowserSync(),
+    qrcode()
+  ],
+}
