04b3ade41886ba366caed618df41ec9f15ef08f5
Гроза Анатолий Русланович <355@sfx-tula.ru>
2024-02-21 15:51:13 +0300

    u21.02.2024

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,11 +1,18 @@
 import "../scss/main.scss";
 
 // import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle";
 
 // import styles bundle
-import 'swiper/css/bundle';
+import "swiper/css/bundle";
 
-const swiper = new Swiper(".swiperr", {
-    slidesPerView:1
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 4,
+  navigation: {
+    nextEl: ".swiper-button_next",
+    prevEl: ".swiper-button_prev",
+  },
+  loop: true,
+  slideClass: "main-slider_item",
+  spaceBetween: 100,
 });
